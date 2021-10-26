557e92d0d5bd974ff04b31b762ad4ebca16bf5c9
Mateusz M <mateo9656@gmail.com>
2021-10-27 00:46:20 +0200

    Add reserve switcher

---
+++ FlyyAirlines-MVC/wwwroot/js/ReserveSwitcher.js
@@ -0,0 +1,17 @@
+﻿const reserveLayout = document.getElementById("quickReserve");
+
+const getToggle = document.getElementById("toggleSwitcher");
+
+getToggle.checked = false;
+
+function toggleSwitch() {
+    if (getToggle.checked == true) {
+        reserveLayout.style.display = "block";
+    }
+    else {
+        reserveLayout.style.display = "none";
+    }
+}
+
+getToggle.addEventListener("click", toggleSwitch);
+
