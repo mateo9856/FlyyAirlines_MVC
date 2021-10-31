4aa72f2e2846a36b59ddabc017b85a15f668eba6
Mateusz M <mateo9656@gmail.com>
2021-10-31 17:22:55 +0100

    add dictionary errors

---
+++ FlyyAirlines-MVC/Models/ErrorDictionary/ErrorsDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyyAirlines_MVC.Models.ErrorDictionary
+{
+    public static class ErrorsDictionary
+    {
+        public static Dictionary<string, string> ErrorsList = new Dictionary<string, string>()
+        {
+            {"Forbidden", "Brak uprawnień do tego modułu/elementu"},
+            {"Unauthorized", "Nie jesteś zalogowany!"},
+            {"", ""}
+        };
+    }
+}
