35956ea06ea308fa4670d53515144dd2377a6ecf
Mateusz M <mateo9656@gmail.com>
2021-11-02 23:29:39 +0100

    Conditional to check if user have a permission

---
+++ FlyyAirlines-MVC/Models/StaticModels/Authorization.cs
@@ -0,0 +1,17 @@
+﻿using FlyyAirlines.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyyAirlines_MVC.Models.StaticModels
+{
+    public static class Authorization
+    {
+        public static bool Can(string Permission, User user)
+        {
+            return user.Permissions.Any(d => d.Name == Permission);
+        }
+
+    }
+}
