71a3e7a4850b1481e0909efad4955f2d560beec0
Mateusz M <mateo9656@gmail.com>
2021-11-09 17:26:43 +0100

    Employee View

---
+++ FlyyAirlines-MVC/Models/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FlyyAirlines.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyyAirlines_MVC.Models
+{
+    public class EmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string WorkPosition { get; set; }
+        public List<Permission> EmployeePermissions { get; set; }
+    }
+}
