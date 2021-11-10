13fcb9b00ae28e6ba64855c530daabcfb1f6e391
Mateusz M <mateo9656@gmail.com>
2021-11-10 17:28:18 +0100

    add viewModel to permissionAdd To user

---
+++ FlyyAirlines-MVC/Models/PermissionManagerModel.cs
@@ -0,0 +1,16 @@
+﻿using FlyyAirlines.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyyAirlines_MVC.Models
+{
+    public class PermissionManagerModel
+    {
+        public List<Permission> Permissions { get; set; }
+        public List<User> Users { get; set; }
+        public string[] SelectedPermissions { get; set; }
+        public string[] SelectedUsers { get; set; }
+    }
+}
