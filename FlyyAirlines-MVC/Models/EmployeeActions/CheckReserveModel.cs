ebc8cfa12a6b4c32d5574aafa2731fd1c1e39f2c
Mateusz M <mateo9656@gmail.com>
2021-11-13 17:17:23 +0100

    Add Model

---
+++ FlyyAirlines-MVC/Models/EmployeeActions/CheckReserveModel.cs
@@ -0,0 +1,20 @@
+﻿using FlyyAirlines.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyyAirlines_MVC.Models.EmployeeActions
+{
+    public class CheckReserveModel
+    {
+        public long? PersonIdentify { get;set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ReservationId { get; set; }
+        public int? Seat { get; set; }
+        public string FlightName { get; set; }
+        public string AirplaneName { get; set; }
+        public bool? IsChecked { get; set; }
+    }
+}
